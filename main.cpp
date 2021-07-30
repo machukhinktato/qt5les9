ef86f5772676e58827bd6690a8484c493b50fcbb
machukhinktato <tarabrinmv@gmail.com>
2021-07-30 18:24:33 +0300

    work with hotkeys in application

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include <QApplication>
 #include "click.h"
-
+#include "keypress.h"
 
 int main(int argc, char *argv[]) {
 
     QApplication app(argc, argv);
 
-    Click window;
+//    Click window;
+    KeyPress window;
 
     window.resize(250,150);
-    window.setWindowTitle("Click");
+    window.setWindowTitle("machukhinktato application");
     window.show();
 
     return QApplication::exec();
